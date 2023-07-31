3c02ae807a2cb199e0821c3141ce15be29afef41
SarahEGreer <140839981+SarahEGreer@users.noreply.github.com>
2023-07-31 11:32:45 -0400

    Dry code & fix view height

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-let homeScoreEl = document.getElementById("home-score");
-let guestScoreEl = document.getElementById("guest-score");
-let homeScoreBtnOne = document.getElementById("hm-add-one");
-let homeScoreBtnTwo = document.getElementById("hm-add-two");
-let homeScoreBtnThree = document.getElementById("hm-add-three");
+const homeScoreEl = document.getElementById("home-score");
+const guestScoreEl = document.getElementById("guest-score");
+const homeScoreBtnOne = document.getElementById("hm-add-one");
+const homeScoreBtnTwo = document.getElementById("hm-add-two");
+const homeScoreBtnThree = document.getElementById("hm-add-three");
 let homeScore = 0
 let guestScore = 0
 
-function addHmScoreOne() {
-    homeScore += 1
+
+function addHomeScore(score) {
+    homeScore += score
     homeScoreEl.textContent = homeScore
 }
 
-function addHmScoreTwo() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-}
 
-function addHmScoreThree() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-}
-
-function addGuestScoreOne() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
-}
-
-function addGuestScoreTwo() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
-}
-
-function addGuestScoreThree() {
-    guestScore += 3
+function addGuestScore(score) {
+    guestScore += score
     guestScoreEl.textContent = guestScore
 }
